acfaad1dcb9dde9aa38863939b148d8aa7088497
Julia Pesola <jcpesola@gmail.com>
2024-06-28 17:16:46 -0400

    Created list of book genres from data set

--- data.py
+++ data.py
@@ -2,8 +2,24 @@ from book_class import Book
 import csv
 
 library = []
-
-with open('Computer Science /Book Recomender/library.csv', newline='') as csvfile:
+genre_collection = []
+with open('library.csv', newline='') as csvfile:
     book_reader = csv.DictReader(csvfile, delimiter=',')
     for row in book_reader:
-        print(row['title'])
+        #check if genres exists in list of genres. If not on list, add genre to list
+        genres = row['genres'].rsplit("'")
+        for genre in genres:
+           clean_genre = genre.rsplit("'")
+           for genre in clean_genre:
+              genre.strip()
+              genre.strip("'")
+              genre.strip("[")
+              genre.strip("]")
+              if genre not in genre_collection:
+                    genre_collection.append(genre)
+    
+        #create a book object
+        #add book object to library
+
+print(genre_collection)
+        
