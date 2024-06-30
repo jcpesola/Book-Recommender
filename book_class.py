4d8fd5e536654c51d845d55c3f4948b313e48d94
Julia Pesola <jcpesola@gmail.com>
2024-06-30 17:00:02 -0400

    Created dictionary for books and genres

--- book_class.py
+++ book_class.py
@@ -1,5 +1,8 @@
+import uuid
+
 class Book:
     def __init__(self, title, series, author, rating, description, genres):
+        self.uuid = uuid.uuid1()
         self.title = title
         self.series = series
         self.author = author
@@ -8,7 +11,7 @@ class Book:
         self.genres = genres
     
     def __repr__(self) -> str:
-        description = "The book {title} by {author} has a rating of {rating} and is categorized as {genres}. The description is as follows: {description}.".format(title=self.title, author=self.author, rating=self.rating, genres=self.genres, description=self.description)
+        description = "[The book {title} by {author} has a rating of {rating} and is categorized as {genres}. The description is as follows: {description}.".format(title=self.title, author=self.author, rating=self.rating, genres=self.genres, description=self.description)
         return description
     
 # test = Book("Hunger Games", "Hunger Games", "Suzanne Collins", "5.0", "In a post-apocolyptic world, Katniss tries to save her sister", ["teen", "fantasy", "dystopian"])
--- data.py
+++ data.py
@@ -1,64 +1,31 @@
+from typing import Dict
 from book_class import Book
 import csv
-from hashmap import HashMap
+import uuid
 
-def old():
-    library = []
-    genre_collection = []
-    with open('library.csv', newline='') as csvfile:
-        book_reader = csv.DictReader(csvfile, delimiter=',')
-        for row in book_reader:
-            #check if genres exists in list of genres. If not on list, add genre to list
-            book_genres = []
-            genres = row['genres'].rsplit("'")
-            for genre in genres:
-                clean_genre = genre.rsplit("'")
-                for genre in clean_genre:
-                    genre.strip()
-                    genre.strip("'")
-                    genre.strip("[")
-                    genre.strip("]")
-                    if genre not in genre_collection:
-                            genre_collection.append(genre)
-                            book_genres.append(genre)
-                    
-        
-            #create a book object
-            inst_name = row['title'].strip()
-            globals()[inst_name] = Book(row['title'], row['series'],row['author'], row['rating'], row['description'], book_genres)
-            library.append(globals()[inst_name]) 
-            #add book object to library
-
-def new():
-    genre_collection = set()
-    book_count = 0
-    library_count = 60000
-    library = HashMap(library_count)
+genre_collection: Dict[str, set] = dict()
+book_count = 0
+library_count = 60000
+library: Dict[uuid.UUID, Book] = dict()
 
 
-    with open('library.csv', newline='') as csvfile:
-        book_reader = csv.DictReader(csvfile, delimiter=',')
-        for row in book_reader:
-            book_count += 1
-            #create list of genres
-            genre_list = row["genres"].strip("[]").replace("'", "").split(", ")
+with open('library.csv', newline='') as csvfile:
+    book_reader = csv.DictReader(csvfile, delimiter=',')
+    for row in book_reader:
+        book_count += 1
+        #create list of genres
+        genre_list = row["genres"].strip("[]").replace("'", "").split(", ")
 
-            #add genres to genre set
-            for genre in genre_list:
-                genre_collection.add(genre)
-
-            #create hash for each each book object
-            title_key = row["title"].replace(" ", "")
-
-            #create object for each book
-            book_object = Book(row['title'], row['series'],row['author'], row['rating'], row['description'], genre_list)
+        #create object for each book
+        book_object = Book(row['title'], row['series'],row['author'], row['rating'], row['description'], genre_list)
+        library.update({book_object.uuid: book_object})
             
-            #create hashmap of book objects
-            library.assign(title_key, book_object)
+        #add genres and book UUID's to genre dictionary
+        for genre in genre_list:
+            if genre not in genre_collection:
+                genre_collection[genre] = set()
+            genre_collection[genre].add(book_object.uuid)
 
+            
 
-    print(library.retrieve("thehungergames"))
-
-new()
-
-        
+    
